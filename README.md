a4961862b166af3f03842d82bcaba22479d89804
Jamie Davis <jrdavismaine@gmail.com>
2026-05-10 14:52:41 +0100

    feature: Added CDP support

--- README.md
+++ README.md
@@ -5,30 +5,29 @@ A repl used to interact with playwright.
 # Install
 
 ```
-git clone https://github.com/jdavis61/playwright-repl.git
+git clone https://github.com/jrdavismaine/playwright-repl.git
 cd playwright-repl
-yarn
+npm install
+npx playwright install
 ```
 
 # Load REPL in headed mode.
 
 ```
-yarn repl --headed
 npm run repl -- --headed
 ```
 
 # Load REPL in headless mode.
 
 ```
-yarn repl
 npm run repl
 ```
 
 # Interact with playwright
 
-## Chrome
+The following sections explain how to load chromium, webkit, firefox or connect to a chromium CDP sesssion.
 
-Enter these commands into the REPL.
+## Chrome
 
 ```
 .chromium
@@ -38,8 +37,6 @@ await page.screenshot({path: 'google.png'});
 
 ## Firefox
 
-Enter these commands into the REPL.
-
 ```
 .firefox
 await page.goto('https://www.google.com');
@@ -48,10 +45,41 @@ await page.screenshot({path: 'google.png'});
 
 ## Webkit
 
-Enter these commands into the REPL.
-
 ```
 .webkit
 await page.goto('https://www.google.com');
 await page.screenshot({path: 'google.png'});
+```
+
+## CDP
+
+Connect to a chromium browser instance using the CDP protocol. Set the CDP environment variable to connect to a remote CDP browser session. If not set, the default connection will be localhost:9229. Note that this feature only connects to the first tab of the first active CDP context.
+
+```
+.cdp
+await page.title();
+await page.screenshot({path: 'cdp.png'});
+```
+
+## Other CLI options
+
+| Command | Default Value | Description                    |
+| ------- | ------------- | ------------------------------ |
+| timeout | 3000 ms       | Set global timeout             |
+| url     | Optional      | Pass URL for initial page load |
+
+## Examples
+
+### timeout 10 seconds
+
+```
+npm run repl -- --headed --timeout 10000
+.webkit
+```
+
+### Load page
+
+```
+npm run repl -- --headed  --url http://127.0.0.1:8080/test.html
+.chromium
 ```
--- repl.js
+++ repl.js
@@ -1,49 +1,58 @@
-const repl = require('node:repl');
-const { program } = require('commander');
-const { chromium, firefox, webkit } = require('playwright'); 
-const init = require('./init');
+const repl = require("node:repl");
+const { program } = require("commander");
+const { chromium, firefox, webkit } = require("playwright");
+const init = require("./init");
+const initCDP = require("./initConnectCDP.js");
 
-program.option('--headed');
+program.option("--headed");
 program.parse();
 const opts = program.opts();
 const isHeaded = opts.headed;
 
-repl.builtinModules = [
-  'playwright',
-];
+repl.builtinModules = ["playwright"];
 
-const replServer = repl.start({ useColors: true, prompt: 'playwright-repl> ' });
+const replServer = repl.start({ useColors: true, prompt: "playwright-repl> " });
 
-replServer.defineCommand('chromium', {
-  help: 'Load chromium',
+replServer.defineCommand("chromium", {
+  help: "Load chromium",
   async action() {
     this.clearBufferedCommand();
     const page = await init(chromium, isHeaded);
     this.context.page = page;
     this.displayPrompt();
-  }
+  },
 });
 
-replServer.defineCommand('firefox', {
-  help: 'Load firefox',
+replServer.defineCommand("cdp", {
+  help: "Connect to a chromium CDP session.",
+  async action() {
+    this.clearBufferedCommand();
+    const page = await initCDP(chromium);
+    this.context.page = page;
+    this.displayPrompt();
+  },
+});
+
+replServer.defineCommand("firefox", {
+  help: "Load firefox",
   async action() {
     this.clearBufferedCommand();
     const page = await init(firefox, isHeaded);
     this.context.page = page;
     this.displayPrompt();
-  }
+  },
 });
 
-replServer.defineCommand('webkit', {
-  help: 'Load webkit',
+replServer.defineCommand("webkit", {
+  help: "Load webkit",
   async action() {
     this.clearBufferedCommand();
     const page = await init(webkit, isHeaded);
     this.context.page = page;
     this.displayPrompt();
-  }
+  },
 });
 
-replServer.on('exit', () => {
+replServer.on("exit", () => {
   process.exit();
 });
